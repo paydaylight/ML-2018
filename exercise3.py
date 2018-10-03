b77353f1420dc64941f16c593d812fa28d0ec763
Maxat Sangerbayev <maxat235@gmail.com>
2018-10-03 13:31:13 +0600

    Create exercise3.py

---
+++ exercise3.py
@@ -0,0 +1,15 @@
+from scipy.optimize import minimize
+import numpy
+import math
+import matplotlib.pyplot as plt
+
+def F(x): #наша функция
+    return math.sin(x/5)*math.exp(x/10)+5*math.exp(-x/2)
+
+x = numpy.arange(1, 30, .1) #точки графика
+mini = minimize(F, 2, method='BFGS') #подсчитываем по методу BFGS для 2
+print(round(F(float(mini.x)), 2)) #первый ответ
+mini = minimize(F, 30, method='BFGS')
+print(round(F(float(mini.x)), 2)) #второй ответ
+
+plt.plot(x, list(map(F, x)))#рисуем график
