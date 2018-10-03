64293c4d6778bf259fdfa4bb3ac278f13e38e30b
Maxat Sangerbayev <maxat235@gmail.com>
2018-10-03 11:11:32 +0600

    Update exercise2.py

--- exercise2.py
+++ exercise2.py
@@ -1,3 +1,8 @@
+# coding: utf-8
+
+# In[46]:
+
+
 import math
 import numpy
 import matplotlib.pyplot as plt
@@ -5,27 +10,43 @@ import matplotlib.pyplot as plt
 def F(x): #наша функция
     return math.sin(x/5)*math.exp(x/10)+5*math.exp(-x/2)
 
-def solveMatrix(arr): #функция для решения матрицы
-    matrixCoef = numpy.zeros(shape=(len(arr), len(arr))) #матрица коэфицентов
-    matrixAnsw = [] #матрица ответов
-    for i in range(len(arr)): #в зависимости от размера матрицы заполняем коэффиценты
-        matrixAnsw.append(F(arr[i])) #добавляем в матрицу ответов решения
+def matrix(arr): #создаем матрицу размерности nxn
+    matrixCoef = numpy.zeros(shape=(len(arr), len(arr)))
+    for i in range(len(arr)):
         for j in range(len(arr)):
-            matrixCoef[i][j] = arr[i]**j #подсчитываем коэффиценты
-    return numpy.linalg.solve(matrixCoef, matrixAnsw) #возвращяем решения
+            matrixCoef[i][j] = arr[i]**j
+    return matrixCoef
 
+def sumOfEle(arr, x): #подсчитываем сумму прогрессии нужной нам размерности
+    out = numpy.zeros(shape=(len(x)))
+    for i in range(len(x)):
+        for j in range(len(arr)):
+            out[i] += arr[j] * (x[i]**j)
+    return out
+
+x = numpy.arange(1, 15, .1) #наш набор точек от 1 до 15
 in1 = [1, 15]
 in2 = [1, 8, 15]
 in3 = [1, 4, 10, 15]
-#вызываем функцию от инпутов
-y1 = solveMatrix(in1)
-y2 = solveMatrix(in2)
-y3 = solveMatrix(in3)
 
-print(y1)
-print(y2)
-print(y3)
-#рисуем график
-plt.plot(in1, y1)
-plt.plot(in2, y2)
-plt.plot(in3, y3)
+m1 = matrix(in1)
+m2 = matrix(in2)
+m3 = matrix(in3)
+#лист решений функции F(x)
+l1 = list(map(F, in1)) 
+l2 = list(map(F, in2))
+l3 = list(map(F, in3))
+
+answ1 = numpy.linalg.solve(m1, l1)
+answ2 = numpy.linalg.solve(m2, l2)
+answ3 = numpy.linalg.solve(m3, l3)
+#подсчитываем Y
+y = list(map(F, x))
+y1 = sumOfEle(answ1, x)
+y2 = sumOfEle(answ2, x)
+y3 = sumOfEle(answ3, x)
+#рисуем графики
+plt.plot(x, y)
+plt.plot(x, y1)
+plt.plot(x, y2)
+plt.plot(x, y3)
